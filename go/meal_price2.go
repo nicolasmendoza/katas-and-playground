b3dae36a50914f522b763eb251cc90c122df2684
Nicolás Mendoza <nicolas.mendoza@yandex.com>
2017-09-05 09:10:19 -0300

    Recovering meal_price2.go

--- go/meal_price2.go
+++ go/meal_price2.go
@@ -4,16 +4,12 @@ import (
 	"fmt"
 )
 
-const (
-	pi = 3.1416
-)
-
 func main(){
 	var cost, tipPercent, taxPercent float64
 	fmt.Scanln(&cost)
 	fmt.Scanln(&tipPercent)
 	fmt.Scanln(&taxPercent)
-	fmt.Println(pi)
+
 	tip:= cost * (tipPercent/100.0)
 	tax := cost * (taxPercent/100.0)
 	fmt.Printf("The total meal cost is %.0f dollars.", cost+tip+tax)
